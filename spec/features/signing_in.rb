40684f0cb37a09580cdd2745ac0e004c8a929f10
Jen Haines <motochick67@gmail.com>
2014-11-09 14:24:04 -0800

    Fixed Rspec Issue

--- spec/features/signing_in.rb
+++ spec/features/signing_in.rb
@@ -2,11 +2,6 @@ require 'rails_helper'
 
 feature 'Visitor sign in flow' do
 
-  # before do
-  #   @user = create(:user)
-  #   @user2 = create(:user)
-  #   @user3 = create(:user)
-  # end
 
   scenario 'with valid email and password' do
     user = create(:user)
@@ -17,19 +12,21 @@ feature 'Visitor sign in flow' do
     expect( page ).to have_content('Signed in successfully')
   end
 
-  # scenario 'with invalid email' do
-  #   visit new_user_session_path
-  #   fill_in 'Email', with: 'papa@invalid.com'
-  #   fill_in 'Password', with: @user2.password
-  #   click_button 'Log in'
-  #   find_link('Sign In').visible?
-  # end
+  scenario 'with invalid email' do
+    user = create(:user)
+    visit new_user_session_path
+    fill_in 'Email', with: 'papa@invalid.com'
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+    find_link('Sign In').visible?
+  end
 
-  # scenario 'with blank password' do
-  #   visit new_user_session_path
-  #   fill_in 'Email', with: @user3.email
-  #   click_button 'Log in'
-  #   find_link('Sign In').visible?
-  # end
+  scenario 'with blank password' do
+    user = create(:user)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    click_button 'Log in'
+    find_link('Sign In').visible?
+  end
   
 end
