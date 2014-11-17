a20cedc17ab1a2be5d9bcc66bbc0044b36f8f4d6
Xander Miller <xander.miller@gmail.com>
2014-11-17 19:42:56 +0000

    Correctly displays date Todo should expire. Adds expires_at method to Todos.

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,5 +3,9 @@ class Todo < ActiveRecord::Base
   belongs_to :user
 	validates :description, presence: true
 
-	 default_scope { where(:complete => false).order('created_at DESC')}
+	default_scope { where(:complete => false).order('created_at DESC')}
+
+  def expires_at
+    created_at + 1.week
+  end
 end
